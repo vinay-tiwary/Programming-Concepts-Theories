ebb76cafc0043a0a546deafbbe0172623bc4dc64
vinay-tiwary <vinay.k.tiwary@gmail.com>
2016-12-06 14:55:41 +0530

    Create PrimeNumber.c
    check prime number. Time complexity of this solution is O(√n)

---
+++ PrimeNumber.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int isPrime(int n)
+{
+        // Corner cases
+        if (n <= 1)  return 0;
+        if (n <= 3)  return 1;
+
+// This is checked so that we can skip 
+// middle five numbers in below loop
+        if (n%2 == 0 || n%3 == 0) return 0;
+
+        for (int i=5; i*i<=n; i=i+6)
+                if (n%i == 0 || n%(i+2) == 0)
+                        return 0;
+
+        return 1;
+}
+    
+    
+// Driver Program to test above function
+int main()
+{   
+        isPrime(11)?  printf("true\n"): printf("false\n");
+        isPrime(15)?  printf("true\n"): printf("false\n");
+        return 0;
+}
