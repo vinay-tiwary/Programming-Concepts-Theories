10e37ac677d0e4956ab16e41e7e084f862bcc98d
Vinay Tiwary <vinay.k.tiwary@gmail.com>
2017-02-20 23:26:01 +0530

    Adding mutex lock in thread program.

--- Multithreading/thread.c
+++ Multithreading/thread.c
@@ -15,7 +15,7 @@ pthread_mutex_t lock;
 void *myThreadFun(void *vargp)
 {
 	// Store the value argument passed to this thread
-	int i, lock_status;
+	int i, lock_status,j;
 	tdata *input = (tdata *)vargp;
 
 	lock_status = pthread_mutex_lock(&lock);
@@ -34,10 +34,15 @@ void *myThreadFun(void *vargp)
 int main(int argc, char **argv)
 {
 	int i,rc;
-	tdata input[2];
-	pthread_t tid;
+	tdata input[NUM_THREADS];
+	pthread_t tid[NUM_THREADS];
 
 	pthread_mutex_init(&lock, NULL);
+
+	if ( argc == 1 ) {
+		printf("Error: Pass total number of iterations\n");
+		exit(1);
+	}
 	
 	printf("%d, %s\n",argc,argv[1]);
 	
@@ -46,7 +51,7 @@ int main(int argc, char **argv)
 		input[i].id = i;
 		input[i].total = atoi(argv[1]);
 		printf("Thread %d is starting..\n",input[i].id);
-		if (rc = pthread_create(&tid, NULL, myThreadFun, &input[i])) {
+		if (rc = pthread_create(&tid[i], NULL, myThreadFun, &input[i])) {
 			printf("Error: pthread_create, rc: %d\n", rc);
 			return EXIT_FAILURE;
 		}
@@ -54,7 +59,7 @@ int main(int argc, char **argv)
 
 	/* block until all threads complete */
 	for (i = 0; i < NUM_THREADS; i++) {
-//		pthread_join(input[i].id, NULL);
+		pthread_join(tid[i], NULL);
 	}
 	return 0; 
 }
